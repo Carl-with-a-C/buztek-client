f3cb7c96f1be0a0509607da2b45d0053e0bda525
Carl-with-a-C <carlbclifton@gmail.com>
2023-03-26 13:04:40 +0100

    refactored pre-loader, extracted into components folder

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,7 @@ import Landing from "./components/Landing";
 import Services from "./components/Services";
 import Nav from "./components/Nav";
 import NavButton from "./components/NavButton";
-
-import LongArrow from "./assets/Arrow---Right.svg";
+import PreLoader from "./components/PreLoader";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -370,51 +369,7 @@ function App() {
       </div>
       <Nav navOpen={navOpen} />
       <NavButton navOpen={navOpen} setNavOpen={setNavOpen} />
-
-      {/*PRELOADER----------------------------------------*/}
-      <main className="pre-loader">
-        <div className="pre-loader-container">
-          <div className="wordmark-container">
-            <div className="wordmark">BUZTEK</div>
-          </div>
-          <div className="header concat">
-            <h1 data-text="Our expertise lies in">Our expertise lies in</h1>
-            <div className="header-wrapper"></div>
-          </div>
-          <div className="header">
-            <h1>translating your core </h1>
-            <div className="header-wrapper"></div>
-          </div>
-          <div className="header">
-            <h1> needs, values and strategy</h1>
-            <div className="header-wrapper"></div>
-          </div>
-          <div className="header">
-            <h1> into inspiring</h1>
-            <div className="header-wrapper"></div>
-          </div>
-          <div className="header">
-            <h1 id="tech-solutions" data-text="technical solutions">
-              technical solutions
-            </h1>
-            <div className="header-wrapper"></div>
-          </div>
-
-          <div
-            className="pre-loader-btn"
-            onClick={() => setRevealSite(!revealSite)}
-          >
-            <div className="btn">
-              Click anywhere
-              <img src={LongArrow} alt="right arrow" />
-              to start a{" "}
-              <span id="buzz" data-text="Buzz">
-                Buzz
-              </span>
-            </div>
-          </div>
-        </div>
-      </main>
+      <PreLoader revealSite={revealSite} setRevealSite={setRevealSite} />
     </div>
   );
 }
